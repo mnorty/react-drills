e9863b494da808cd35cc1e9d08e411936989bb6f
mnorty <mnorty@gmail.com>
2019-06-20 15:32:16 -0600

    app4 is done

---
+++ app-4/src/Login.js
@@ -0,0 +1,38 @@
+import React,{Component} from 'react'
+
+class Login extends Component{
+  constructor(){
+    super()
+
+    this.state = {
+      username: '',
+      password: ''
+    }
+
+    this.handleLogin = this.handleLogin.bind(this)
+  }
+
+  handleUserNameChange(name) {
+    this.setState({username:name})
+  }
+  handlePasswordChange(password) {
+    this.setState({password:password})
+  }
+
+  handleLogin() {
+    alert(`Username: ${this.state.username} Password: ${this.state.password}`)
+  }
+
+  render() {
+    return (
+      <div>
+        <input onChange={e => this.handleUserNameChange(e.target.value)} type="text"/>
+        <input onChange={e => this.handlePasswordChange(e.target.value)} type="text"/>
+        <button onClick={this.handleLogin}>Login</button>
+      </div>
+    )
+  }
+}
+
+export default Login
+
